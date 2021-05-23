313da277b993c362a6b7099c95c2a4eddc8804fe
Jayprakash Patil <siddharth.verma@invimatic.com>
2021-05-23 21:39:49 +0530

    mysql demo app

--- database.js
+++ database.js
@@ -3,7 +3,7 @@ import mysql from 'mysql'
 const connection=mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'admin',
+    password: '',
     database: 'matrinomial_db'
 })
 export const connectt= async ()=>   {
