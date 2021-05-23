accc25687840714b426d30d226c5ba1147a1b180
Jayprakash Patil <siddharth.verma@invimatic.com>
2021-05-23 21:54:12 +0530

    mysql demo app

--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,13 @@
-// import * as db from '../database.js'
+import * as db from '../database.js'
 import express from 'express'
 
 const router=express.Router()
 
 router.get('/', async (req, res)=>  {
-    return res.status(200).json({ message: 'Successfully fetched the users' })
+    db.query('select * from users',(err, results, fields)=> {
+        return res.status(200).json({ message: 'Successfully fetched the users', users: results })
+    })
+    
 })
 
 export default router
